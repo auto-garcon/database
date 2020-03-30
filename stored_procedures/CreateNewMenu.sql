7ff58af0054778ba7ef71c9a4cb742c84678843f
Tyler Beverley <tylerbeverley@gmail.com>
2020-03-30 08:12:59 -0500

    inserts a time range, a menu, and gives the menuID back.

--- stored_procedures/CreateNewMenu.sql
+++ stored_procedures/CreateNewMenu.sql
@@ -1,5 +1,2 @@
-CREATE DEFINER=`masterUser`@`%` PROCEDURE `CreateNewMenu`(IN startTime TIME, IN endTime time, IN mStatus INT, in  menuName varchar(255), IN restaurantID INT)
-BEGIN
-	INSERT into Menu(startTime, endTime, menuStatus, menuName, restaurantID)
-    VALUES (startTime, endTime, mStatus, menuName, restaurantID);
-END
+CREATE PROCEDURE CreateNewMenu(IN startTime TIME, IN endTime time, IN mStatus INT, in  menuName varchar(255), IN restaurantID INT, OUT menuID INT)
+BEGIN INSERT INTO Menu(menuStatus, menuName, restaurantID) VALUES(mStatus, menuName, restaurantID); INSERT INTO MenuTimes( menuID, startTime, endTime) VALUES( (SELECT LAST_INSERT_ID() FROM Menu ), startTImeDD, endTime);  SELECT LAST_INSERT_ID() INTO menuID From Menu;  END
