e82bed058c81138f9a6450c0e87c67f120253efc
Tyler Reiland <31970732+tylerreiland@users.noreply.github.com>
2020-03-31 18:14:23 -0500

    revert to selecting menuID instead of output param

--- stored_procedures/CreateNewMenu.sql
+++ stored_procedures/CreateNewMenu.sql
@@ -1,9 +1,14 @@
 DELIMITER //
-CREATE PROCEDURE CreateNewMenu(IN startTime INT, IN endTime INT, IN mStatus INT, IN  menuName varchar(255), IN restaurantID INT, OUT menuID INT) 
-  BEGIN 
+CREATE PROCEDURE CreateNewMenu(IN startTime INT, IN endTime INT, IN mStatus INT, IN  menuName varchar(255), IN restaurantID INT)
+BEGIN 
+	DECLARE createdMenuID INT;
+    
     INSERT INTO Menu(menuStatus, menuName, restaurantID) 
-     VALUES(mStatus, menuName, restaurantID); 
-    SELECT LAST_INSERT_ID() INTO menuID; 
+    VALUES(mStatus, menuName, restaurantID); 
+    SELECT LAST_INSERT_ID() INTO createdMenuID;
+    
     INSERT INTO MenuTimes( menuID, startTime, endTime) 
-     VALUES( (SELECT LAST_INSERT_ID() ), startTime, endTime);   
-     END //
+    VALUES( (SELECT LAST_INSERT_ID() ), startTime, endTime); 
+    
+    SELECT createdMenuID;
+END
