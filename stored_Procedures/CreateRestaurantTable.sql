2e8a287a990b9727fc7eae0ed5e496caa732a924
Mugdha Danda <43558108+Mugdha12@users.noreply.github.com>
2020-05-06 22:30:06 -0500

    Delete CreateRestaurantTable.sql

--- stored_Procedures/CreateRestaurantTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`masterUser`@`%` PROCEDURE `CreateRestaurantTable`(IN alexaID VARCHAR(100), IN qrCode VARCHAR(100), IN restaurantID INT)
-BEGIN
-	INSERT INTO RestaurantTables(alexaID, qrCode, restaurantID)
-    VALUES (alexaID, qrCode, restaurantID);
-    
-    SELECT LAST_INSERT_ID() AS newTableID;
-END
