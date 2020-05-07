c047741bacfcf35afd1fcf8ffdcd5f94609f8705
Mugdha Danda <43558108+Mugdha12@users.noreply.github.com>
2020-05-06 22:31:13 -0500

    Delete GetOptionsForOrderItem.sql

--- stored_Procedures/GetOptionsForOrderItem.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`masterUser`@`%` PROCEDURE `GetOptionsForOrderItem`(IN oItemID INT)
-BEGIN
-	SELECT *
-    FROM Options
-    WHERE optionID IN (SELECT OrderItemHasOptions.optionID FROM OrderItemHasOptions WHERE OrderItemHasOptions.orderItemID = oItemID);
-END
