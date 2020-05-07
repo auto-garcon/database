dd33098dc0bb4bb3dbf2c2b71c9fa05be8b0d52a
Mugdha Danda <43558108+Mugdha12@users.noreply.github.com>
2020-05-06 22:30:34 -0500

    Delete GetAllOrderItemsFromOrder.sql

--- stored_Procedures/GetAllOrderItemsFromOrder.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`masterUser`@`%` PROCEDURE `GetAllOrderItemsFromOrder`(IN myOrderID INT)
-BEGIN
-	SELECT OrderItem.orderItemID, MenuItem.*, OrderItem.quantity, OrderItem.price, OrderItem.comments
-	FROM MenuItem JOIN OrderItem ON MenuItem.itemID = OrderItem.menuItemID
-	WHERE orderID = myOrderID;
-END
