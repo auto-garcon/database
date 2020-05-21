7740a03ca717ce7aef46f6c1054865b74184b2c6
Mugdha Danda <43558108+Mugdha12@users.noreply.github.com>
2020-05-21 14:25:50 -0500

    Final Commit

--- stored_procedures/RemoveMenu.sql
+++ stored_procedures/RemoveMenu.sql
@@ -1,5 +1,7 @@
 CREATE DEFINER=`masterUser`@`%` PROCEDURE `RemoveMenu`(IN mID INT)
 BEGIN
+
+	-- Sets the menuStatus for given menu to 0, or inactive --
     UPDATE Menu
     SET menuStatus = 0
     WHERE menuID = mID;
